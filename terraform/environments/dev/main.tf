94cffd0dbe494c2e12268e1ef49d9ceed88ac4c8
foo@kainos.com <foo@kainos.com>
2019-04-16 10:06:17 +0100

    remove service principal details

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -5,7 +5,7 @@ module "dev_aks_cluster" {
   location                = "UK South"
   resource_group_name     = "jmaksjenkinsuk"
   service_principal {
-    app_id = "01187ef1-43fc-4b9a-a804-941dce7b470a"
-    client_secret = "2f0e6ef4-a81e-405b-be2e-12c2173fc9da"
+    app_id = ""
+    client_secret = ""
   }
 }
